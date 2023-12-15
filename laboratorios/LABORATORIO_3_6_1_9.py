af125a2a6191ae58b8fa52b834c79bcdf5cbca14
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-12-15 12:54:07 +0100

    cambios en laboratorio 9

--- laboratorios/LABORATORIO_3_6_1_9.py
+++ laboratorios/LABORATORIO_3_6_1_9.py
@@ -1,6 +1,8 @@
-﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
-for i in my_list:
-	if i in my_list:
-		del my_list[i]
+﻿my_list = [1, 2, 4, 9, 4, 9, 1, 4, 2, 14, 6, 2, 14, 9, 1, 1, 2]
+list = []
+for i in my_list:  
+	if i not in list:  
+		list.append(i)  
+my_list = list[:]  
 print("La lista con elementos únicos:")
 print(my_list)
